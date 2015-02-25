6d992c5c30fa3923f2180a7541d0f2c46cf731ec
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-02-25 17:59:08 +0800

    Input for land problem

--- src/Land.java
+++ src/Land.java
@@ -1,4 +1,20 @@
+import java.util.*;
+
 
 public class Land {
-
+	public void input(){
+		System.out.println("Enter the size of land matrix");
+		Scanner sn = new Scanner(System.in);
+		int N = sn.nextInt();
+		int land[][] = new int[N][N];
+		System.out.println("Enter number of trees");
+		int treeCount = sn.nextInt();
+		int[][] trees = new int[treeCount][2];
+		System.out.println("Enter coordinates for trees");
+		for(int i=0;i<treeCount;i++){
+			trees[i][0]=sn.nextInt();
+			trees[i][1]=sn.nextInt();
+			land[trees[i][0]][trees[i][1]] = 1;			//Assigning 1 to cells inside land where tree exists
+		}
+	}
 }
