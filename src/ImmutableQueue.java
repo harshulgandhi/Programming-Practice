4226857c2da06ed115afacab4ac6ef21b86ee124
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-03-02 23:59:21 +0800

    immutable queue in java using generics

---
+++ src/ImmutableQueue.java
@@ -0,0 +1,32 @@
+//package jp.co.worksap.global;
+import java.util.NoSuchElementException; 
+/** * The Queue class represents 
+ * an immutable first-in-first-out (FIFO)
+ *  queue of objects.  
+ *  @param <E> 
+ */
+
+public class ImmutableQueue<E>{
+	/*
+	 * requires default constructor
+	 * */
+	
+	public ImmutableQueue(){
+		//modify this constructor if necessary, but do not remove default constructor
+		
+	}
+	//add other constructors if necessary
+	
+	/*
+	 * Returns the queue that adds an item into the tail of this queue without modifying this queue
+	 * If the element e is null, throws IllegalArgumentException
+	 * @param e
+	 * @return
+	 * @throws IllegalArgumentException
+	 * */
+	public ImmutableQueue<E> enqueque(E e){
+		
+		return null;
+	}
+	
+}
