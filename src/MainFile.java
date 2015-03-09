b76b4a29616924fb976ecdfa561d517cf8a1ccad
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-03-09 16:31:57 +0800

    Sort matrix in spiral form complete

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,6 +1,7 @@
 import java.awt.List;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 import org.omg.CORBA.portable.ValueBase;
 
@@ -86,6 +87,54 @@ public static void main(String[] args){
 	MatrixSortSpiral msp_obj = new MatrixSortSpiral();
 	msp_obj.input();
 	msp_obj.MatrixToArraySort();
-	
+	/*Scanner sn = new Scanner(System.in);
+	int R = 4, C = 6;
+	int a[][] = new int[R][C];
+	System.out.println("Enter numbers for the 2D array");
+	for (int i=0;i<R;i++){
+		for(int j = 0;j<C;j++){
+			a[i][j] = sn.nextInt();
+		}
+	}
+	int m = R;
+	int n = C;
+	int i, k = 0, l = 0;
+	while (k < m && l < n)
+    {
+         Print the first row from the remaining rows 
+        for (i = l; i < n; ++i)
+        {
+            System.out.print( a[k][i]+"-");
+        }
+        k++;
+ 
+         Print the last column from the remaining columns 
+        for (i = k; i < m; ++i)
+        {
+            System.out.print(a[i][n-1]+"-");
+        }
+        n--;
+ 
+         Print the last row from the remaining rows 
+        if ( k < m)
+        {
+            for (i = n-1; i >= l; --i)
+            {
+                System.out.print(a[m-1][i]+"-");
+            }
+            m--;
+        }
+ 
+         Print the first column from the remaining columns 
+        if (l < n)
+        {
+            for (i = m-1; i >= k; --i)
+            {
+                System.out.print(a[i][l]+"-");
+            }
+            l++;    
+        }        
+    }
+*/	
 }
 }
