522aa7aac9d6cfdb21627415932046d742eb3061
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-05-01 23:12:58 +0800

    Angry professor problem solved

--- src/AngryProf.java
+++ src/AngryProf.java
@@ -2,37 +2,53 @@ import java.util.*;
 
 public class AngryProf {
 	
-	
-	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		Scanner sn = new Scanner (System.in);
 		int numTestCases = sn.nextInt();
-//		System.out.println("Number of test cases : "+numTestCases);
-		ArrayList ans = new ArrayList();
 		for (int i = 0;i<numTestCases;i++){
 			int countStudBefore = 0;
-			Scanner sn1 = new Scanner (System.in);
-			int numStud = sn1.nextInt();
-//			System.out.println("Number of students : "+numStud);
-			int threshold = sn1.nextInt();
-//			System.out.println("Threshold count : "+threshold);
-			Scanner sn2 = new Scanner (System.in);
+			int numStud = sn.nextInt();
+			int threshold = sn.nextInt();
+			
 			for (int j = 0;j<numStud;j++){
-				int time = sn2.nextInt();
-				if(time>=0){countStudBefore++;}
-				if(countStudBefore == threshold){
-					ans.add("NO");
-//					System.out.println("NO");
-					break;
+				int time = sn.nextInt(); 
+				if(time<1){
+					countStudBefore++;
 				}
 			}
 			if(countStudBefore < threshold){
-				ans.add("YES");
-//				System.out.println("YES");
+				System.out.println("YES");
 			}
-		}
-		for (int i = 0;i<ans.size();i++){
-			System.out.println(ans.get(i));
+			else System.out.println("NO");
 		}
 	}
-}
+}	
+	
+	
+	
+	/*
+	 * 
+10
+10 4
+-93 -86 49 -62 -90 -63 40 72 11 67
+10 10
+23 -35 -2 58 -67 -56 -42 -73 -19 37
+10 9
+13 91 56 -62 96 -5 -84 -36 -46 -13
+10 8
+45 67 64 -50 -8 78 84 -51 99 60
+10 7
+26 94 -95 34 67 -97 17 52 1 86
+10 2
+19 29 -17 -71 -75 -27 -56 -53 65 83
+10 10
+-32 -3 -70 8 -40 -96 -18 -46 -21 -79
+10 9
+-50 0 64 14 -56 -91 -65 -36 51 -28
+10 6
+-58 -29 -35 -18 43 -28 -76 43 -13 6
+10 1
+88 -17 -96 43 83 99 25 90 -39 86
+	 * 
+	 * */
+
