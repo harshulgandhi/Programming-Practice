7a38e8c7727e86d8efcfb4052ad3bfd8db946f26
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-05-04 00:48:22 +0800

    Cut the sticks - recursive method final push

--- src/CutSticks.java
+++ src/CutSticks.java
@@ -2,42 +2,50 @@ import java.util.*;
 
 public class CutSticks {
 	
-	public int getMin(ArrayList<Integer> sticks){
+	ArrayList<Integer> sticks = new ArrayList<Integer>();
+	public int getMin(){
 		int min = 1001;
-		for (int i = 0;i<sticks.size();i++){
-			int x =sticks.get(i); 
-			if(x<min && x != 0){
-				min = sticks.get(i);
+		for (int i = 0;i<this.sticks.size();i++){
+			int x =this.sticks.get(i); 
+			if(x<min && x >0){
+				min = this.sticks.get(i);
 			}
 		}
 		return min;
 	}
 	
-	public void cutSticks(ArrayList<Integer> sticks){
+	public void cutSticks(){
 		int countCut = 0;
 		int edgeCount = 0;
-		for (int k = 0;k<sticks.size();k++){
-			if(sticks.get(k)<=0) edgeCount++;
-			if(sticks.get(k)<=0) countCut++;
+		int anyZero = 0;
+		for (int k = 0;k<this.sticks.size();k++){
+			if(this.sticks.get(k)<=0) edgeCount++;
+			if(this.sticks.get(k)>=0) countCut++;
+			if(this.sticks.get(k)==0) anyZero =1;
 		}
-		if(edgeCount == sticks.size()){return;}
+//		System.out.println("countCut :"+countCut);
+//		System.out.println("edgeCount :"+edgeCount);
+		if(anyZero==1){
 		System.out.println(countCut);
-		int min = this.getMin(sticks);
-		for (int j = 0;j<sticks.size();j++){
-			sticks.set(j, sticks.get(j)-min);
 		}
-		cutSticks(sticks);
+		if(edgeCount == this.sticks.size()){return;}
+		
+		int min = this.getMin();
+		for (int j = 0;j<this.sticks.size();j++){
+			this.sticks.set(j, this.sticks.get(j)-min);
+		}
+		this.cutSticks();
 	}
 	
 	public static void main(String[] args) {
 		CutSticks cutObj = new CutSticks();
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		ArrayList<Integer> sticks = new ArrayList<Integer>();
+//		ArrayList<Integer> sticks = new ArrayList<Integer>();
 		for (int i = 0;i<N;i++){
 			int num = sc.nextInt();
-			sticks.add(num);
+			cutObj.sticks.add(num);
 		}
-		
+		cutObj.cutSticks();
 	}
 }
