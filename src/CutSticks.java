b35618ee6e296ec09ebaf986ebda6e853a5ecf96
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-05-03 17:35:34 +0800

    Cut the sticks - recursive solution

--- src/CutSticks.java
+++ src/CutSticks.java
@@ -2,44 +2,42 @@ import java.util.*;
 
 public class CutSticks {
 	
-	ArrayList<Integer> sticks = new ArrayList<Integer>();
-	public int getCountSticksCut(int min){
-		int count = 0;
-		for(int i=0;i<this.sticks.size();i++){
-			this.sticks.set(i, this.sticks.get(i)-min);
-			count++;
-			if(this.sticks.get(i) > 0){
-				count++;
+	public int getMin(ArrayList<Integer> sticks){
+		int min = 1001;
+		for (int i = 0;i<sticks.size();i++){
+			int x =sticks.get(i); 
+			if(x<min && x != 0){
+				min = sticks.get(i);
 			}
 		}
-		return count;
+		return min;
 	}
 	
-	public boolean getStatus(){
-		for (int k = 0;k<this.sticks.size();k++){
-			if(this.sticks.get(k)>0){
-				return true;
-			} 
+	public void cutSticks(ArrayList<Integer> sticks){
+		int countCut = 0;
+		int edgeCount = 0;
+		for (int k = 0;k<sticks.size();k++){
+			if(sticks.get(k)<=0) edgeCount++;
+			if(sticks.get(k)<=0) countCut++;
 		}
-		return false;
+		if(edgeCount == sticks.size()){return;}
+		System.out.println(countCut);
+		int min = this.getMin(sticks);
+		for (int j = 0;j<sticks.size();j++){
+			sticks.set(j, sticks.get(j)-min);
+		}
+		cutSticks(sticks);
 	}
 	
-
 	public static void main(String[] args) {
 		CutSticks cutObj = new CutSticks();
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-//		ArrayList<Integer> sticks = new ArrayList<Integer>();
-//		int min = 1001;
+		ArrayList<Integer> sticks = new ArrayList<Integer>();
 		for (int i = 0;i<N;i++){
 			int num = sc.nextInt();
-//			if(num<min){min = num;}
-			
-			cutObj.sticks.add(num);
+			sticks.add(num);
 		}
-			while(cutObj.getStatus()){
-				int min = Collections.min(cutObj.sticks);
-				System.out.println(cutObj.getCountSticksCut(min));
-		}
+		
 	}
 }
