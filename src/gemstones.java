d26a60aef74862a984fe4347dd856db375acf751
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-05-07 23:47:17 +0800

    Gemstone problem final solution

--- src/gemstones.java
+++ src/gemstones.java
@@ -9,31 +9,21 @@ public static void main(String[] args){
 	int count = 0;
 	for (int k = 0;k<=N;k++){
 		String checkStr = sc.nextLine();
-		
+		ArrayList<Character> processedChar = new ArrayList<Character>();
 		for(int j = 0;j<checkStr.length();j++){
-			System.out.println("value of k -- "+k); 
-			System.out.println("Current char -- "+checkStr.charAt(j));
 			if(hm.get(checkStr.charAt(j)) == null){
-				System.out.println("IF -- ");
-				hm.put(checkStr.charAt(j),0);
-				System.out.println("HashMap ['"+checkStr.charAt(j)+"'] : "+hm.get(checkStr.charAt(j)));
+				hm.put(checkStr.charAt(j),1);
 			}
-			else if(hm.get(checkStr.charAt(j)) == k){
-				System.out.println("ELSE IF -- ");
-				System.out.println("HashMap ['"+checkStr.charAt(j)+"'] : "+hm.get(checkStr.charAt(j)));
+			else if(processedChar.contains(checkStr.charAt(j))){
 				continue;
-				//hm.put(checkStr.charAt(j),hm.get(checkStr.charAt(j)) + 1);
 			}			
 			else{
-				System.out.println("ELSE -- ");
 				hm.put(checkStr.charAt(j),hm.get(checkStr.charAt(j)) + 1);
-				System.out.println("HashMap ['"+checkStr.charAt(j)+"'] : "+hm.get(checkStr.charAt(j)));
 			}
+			processedChar.add(checkStr.charAt(j));
 		}
 	}
-	System.out.println("N :: "+N);
 	for (Character key: hm.keySet()){
-			System.out.println("HashMap ['"+key+"'] : "+hm.get(key));
 			if(hm.get(key)  == N) count++;
 	}
 	System.out.println(count);
