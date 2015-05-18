dbf271dc34baddf6d6aaeda89753dfb235bc6117
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-05-19 01:51:25 +0800

    chocolate feast done, halloween party started

---
+++ src/Warmup/chocolatefeast.java
@@ -0,0 +1,17 @@
+package Warmup;
+import java.util.Scanner;
+public class chocolatefeast {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while(T-->0){
+			int countChoco = 0;
+			int N = sc.nextInt();
+			int C = sc.nextInt();
+			int M = sc.nextInt();
+			countChoco = N/C;
+			
+		}
+	}
+}
