fc3cb744f61fa2256d3d6780ccb874e7bdfe74bc
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-06-03 18:12:25 +0800

    Filling jars complete

---
+++ src/Warmup/fillingjars.java
@@ -0,0 +1,18 @@
+package Warmup;
+import java.util.Scanner;
+
+public class fillingjars {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		double avg = 0.0;
+		while(M-->0){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int k = sc.nextInt();
+			avg += ((double)((b-a)+1)/N)*k;
+		}
+		System.out.println((long)avg);
+	}
+}
