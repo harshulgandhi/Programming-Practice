cf0b6c1a6a376318a4460caeb7034dda54bc9bf1
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-06-21 22:18:38 +0800

    Epiccode hackerrank competition

---
+++ src/HackerrankChallengeEpiccode/BeginEnd.java
@@ -0,0 +1,25 @@
+package HackerrankChallengeEpiccode;
+import java.util.Scanner;
+
+public class BeginEnd {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String inStr = sc.next();
+		int[] ch = new int[26];
+		long ans = 0;
+		for(int i = 0;i<N;i++){
+			ch[inStr.charAt(i) - (int)'a'] += 1; 
+		}
+		
+		for(int i = 0;i<26;i++){
+			if(ch[i] == 1){
+				ans += 1;
+			}
+			else if(ch[i]>1){
+				ans += (((long)ch[i] * (ch[i]-1) / 2) + ch[i]);
+			}
+		}
+		System.out.println(ans);
+	}
+}
