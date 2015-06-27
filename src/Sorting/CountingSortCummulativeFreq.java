da7a435c80b26c4beed6353bfdd350d6b7f2efcd
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-06-27 18:31:17 +0800

    Counting sort intro and advanced, encountered time out issue, need to fix in next commit

---
+++ src/Sorting/CountingSortCummulativeFreq.java
@@ -0,0 +1,39 @@
+package Sorting;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class CountingSortCummulativeFreq {
+	
+	public static void countingSort(int[] freq){
+		int prev = 0;
+		for(int i = 0;i<100;i++){
+			if(i == 0){
+				System.out.print(freq[i]+" ");
+				prev = freq[i];
+			}
+			else{
+				System.out.print((freq[i]+ prev) +" ");
+				prev = freq[i]+ prev;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		HashMap<Integer,List<String>> inputMap = new HashMap<>();
+		int[] freq = new int[100];
+		while(N-->0){
+			int input = sc.nextInt();
+			freq[input]++;
+			if(inputMap.get(input) == null){
+				inputMap.put(input, new ArrayList<String>());
+			}
+			inputMap.get(input).add(sc.next());
+		}
+		
+		countingSort(freq);
+	}
+}
--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,38 +1,5 @@
 package Sorting;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.List;
 
 public class QuickSort {
-	public static void printArr(List<Integer> arr){
-		for(int i = 0;i<arr.size();i++){
-			System.out.print(arr.get(i)+" ");
-		}
-		System.out.println();
-	}
-	
-	static void partition(int[] ar){
-		int pivot = ar[0];
-		List<Integer> left = new ArrayList<Integer>();
-		List<Integer> right = new ArrayList<Integer>();
-		for (int i = 0;i<ar.length;i++){
-			if(ar[i] < pivot) left.add(ar[i]);
-			else if (ar[i] > pivot) right.add(ar[i]);
-		}
-		List<Integer> output = new ArrayList<Integer>();;
-		output.addAll(left);
-		output.add(pivot);
-		output.addAll(right);
-		printArr(output);
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] inputArr = new int[N];
-		for(int i = 0;i<N;i++){
-			inputArr[i] = sc.nextInt();
-		}
-		
-		partition(inputArr);
-	}
+
 }
