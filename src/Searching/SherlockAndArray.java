1d7b065e892545578e0e9038dc7c9ea4a5ccd7dd
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-07-03 11:18:30 +0800

    Initiated Searching problems, SherlockAndWatson problem in sorting

---
+++ src/Searching/SherlockAndArray.java
@@ -0,0 +1,19 @@
+package Searching;
+import java.util.Scanner;
+public class SherlockAndArray {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int T = s.nextInt();
+		while(T-- > 0){
+			int N = s.nextInt();
+			int[] inpArr = new int[N];
+			for(int i = 0;i<N;i++){
+				inpArr[i] = s.nextInt();
+			}
+			
+			for(int i=0,j=N-1;;i++,j--){
+				
+			}
+		}
+	}
+}
