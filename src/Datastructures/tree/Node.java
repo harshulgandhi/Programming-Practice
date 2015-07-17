4677abe824c1b59f44ebb3918a21a535d9290238
Harshul <gandhi.harshul.88@gmail.com>
2015-07-18 01:08:23 +0800

    Simple Binary Tree

--- src/Datastructures/tree/Node.java
+++ src/Datastructures/tree/Node.java
@@ -1,3 +1,5 @@
+package Datastructures.tree;
+
 public class Node{
 	int element;
 	Node left;
---
+++ src/Warmup/BigFactorial.java
@@ -0,0 +1,26 @@
+package Warmup;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+	
+	public BigInteger factorial(long num){
+//		if(num == 1){
+//			return BigInteger.valueOf(1); 
+//		}
+//		BigInteger big = BigInteger.valueOf(1);
+//		big = big.multiply(factorial(num-1));
+//		return big;
+		BigInteger fact = BigInteger.valueOf((long)1);
+		for (long i = num;i>0;i--){
+			fact = fact.multiply(BigInteger.valueOf(i));
+		}
+		return fact;
+	}
+	public static void main(String[] args) {
+		BigFactorial fact = new BigFactorial();
+		Scanner s = new Scanner(System.in);
+		System.out.println(fact.factorial((long)s.nextInt()));
+		
+	}
+}
