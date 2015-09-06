519147c6e13065d01c62136eee04d2dc0b3f208d
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-09-06 13:43:50 +0800

    Linked list - remove element

---
+++ src/CTCI/LinkedList/LinkedList.java
@@ -0,0 +1,23 @@
+public class LinkedList{
+    Node head = null;
+    
+    LinkedList(int val){
+        head = new Node(val);
+    }
+    
+    public void addToRear(Node node){
+        Node head1 = head;
+        while(head1.next != null){
+            head1 = head1.next;
+        }
+        head1.next = node;
+    }
+    
+    public void printLinkedList(){
+        Node head1 = head;
+        while(head1.next != null){
+            System.out.print(head1.val+"-");
+        }
+    }
+    
+}
---
+++ src/CTCI/LinkedList/Node.java
@@ -0,0 +1,8 @@
+public class Node{
+    int val = 0;
+    Node next = null;
+    
+    Node(int val){
+        this.val = val;    
+    }
+}
