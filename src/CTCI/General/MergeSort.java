d50a3e5befd3d816942469ac37728a84aa6e499f
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-09-09 17:28:52 +0800

    Merge sort complete

--- src/CTCI/General/MergeSort.java
+++ src/CTCI/General/MergeSort.java
@@ -4,6 +4,7 @@ public class MergeSort{
 	// int[] arr = {7,6,12,8,9,11,2,13};
 	int[] arr ={7,6,5,4,3,2,1,0};
 	public  void merge(int low1, int high1, int low2, int high2){
+
 		int len = high1-low1+1+high2-low2+1;
 		int[] temp = new int[len];
 		int i = low1;
@@ -17,28 +18,28 @@ public class MergeSort{
 				low1++;
 			}
 			else{
-				// System.out.println("Updating temp at "+k+" with value "+arr[low2]);
+				// System.out.println("Updating temp at "+k+" with value (low2 = "+low2+") "+arr[low2]);
 				temp[k] = arr[low2];
 				low2++;
 			}
 			k++;
 		}
 
-		while(low1 < high1){
-			// System.out.println("Updating temp at "+k+" with value "+arr[low1]);
+		while(low1 <= high1){
+			// System.out.println("Updating temp at "+k+" with value (low1 = "+low1+") "+arr[low1]);
 			temp[k] = arr[low1];
 			k++;
 			low1++;
 		}
 
-		while(low2 < high2){
-			// System.out.println("Updating temp at "+k+" with value "+arr[low2]);
+		while(low2 <= high2){
+			// System.out.println("Updating temp at "+k+" with value (low2 = "+low2+") "+arr[low2]);
 			temp[k] = arr[low2];
 			k++;
 			low2++;
 		}
 		k = 0;
-		while(i < j){
+		while(i <= j){
 			arr[i] = temp[k];
 			i++;
 			k++;
@@ -60,6 +61,7 @@ public class MergeSort{
 		
 		System.out.println();
 		System.out.print("Print before sorting : ");
+		System.out.println();
 		for(int eachNum : obj.arr) System.out.print(eachNum+" ");
 		obj.mergeSort(0, obj.arr.length-1);
 		System.out.println();
