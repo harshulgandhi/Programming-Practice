6badc0585661aecadfcc403708068b4519c1f7f6
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-09-18 01:44:24 +0800

    Some more strings problem

---
+++ src/CTCI/General/StringRevRecursion.java
@@ -0,0 +1,28 @@
+
+
+public class StringRevRecursion{
+
+	public void swap(char[] arr, int i , int j){
+		char temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+	public void reverseRecursion(char[] arr, int i, int j){
+		if(i > j) return;
+		else{
+			reverseRecursion(arr,i+1, j-1);
+			swap(arr, i,j);
+		}
+	}
+
+	public static void main(String[] args){
+		StringRevRecursion obj = new StringRevRecursion();
+		String input = "reverse";
+		char[] input_c = input.toCharArray();
+
+		obj.reverseRecursion(input_c, 0, input.length()-1);
+		System.out.println(new String(input_c));
+
+	}
+}
