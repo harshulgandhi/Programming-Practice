084ab348569f178b03ea4f7cae0b10ae68e4ce1b
harshul gandhi <gandhi.harshul.88@gmail.com>
2015-10-11 20:35:07 +0800

    string - first non repeated

---
+++ src/MorePractice/FirstRepeated.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+public class FirstRepeated{
+
+	public Map getFreqMap(String inp){
+		Map<Character, Integer> freq = new LinkedHashMap<Character, Integer>();
+		for(char c : inp.toCharArray()){
+			if(freq.keySet().contains(c)) freq.put(c, freq.get(c)+1);
+			else freq.put(c, 1);
+		}
+		return freq;
+	}
+
+	public char getRepeatedChar(Map<Character, Integer> freq){
+		for(Character c : freq.keySet()){
+			if (freq.get(c) == 1) return c;
+		}
+		return 'x';
+	}
+
+	public static void main(String[] args){
+		FirstRepeated obj = new FirstRepeated();
+		Map freq = obj.getFreqMap("HeloWorld");
+		System.out.println("First Non Repeated is "+obj.getRepeatedChar(freq));
+	}
+}
