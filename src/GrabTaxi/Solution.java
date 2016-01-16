ad044b07a0a919bc43d9d586c5bb230fe4312473
Harshul <gandhi.harshul.88@gmail.com>
2016-01-16 22:29:06 +0800

    Adding older files to get the ball rolling again

---
+++ src/GrabTaxi/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+	int count = 0;
+	public void check(int[] A, int curr, int next){
+		if(next == A.length) return;
+		check(A, curr, next+1);
+		if(A[next] == curr){
+			count ++;
+		}
+	}
+	
+	 public int solution(int[] A) {
+		 int len = A.length;
+		 for(int i = 0;i<len-1;i++){
+			 check(A, A[i], i+1);
+		 }
+		 
+		 return count;
+	    }
+	 
+ public static void main(String[] args) {
+		Solution obj = new Solution();
+		int[] arr = {1,1,1,5,1,1,5};
+		obj.solution(arr);
+		System.out.println(obj.count);
+		
+	}
+}
