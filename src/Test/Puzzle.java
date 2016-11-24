0f64971965bdf215b249e2fb243d19db2ddd9e4f
Harshul Gandhi <gandhi.harshul.88@gmail.com>
2016-11-25 02:43:56 +0800

    More practice

--- src/Test/Puzzle.java
+++ src/Test/Puzzle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Puzzle {
 	
-	private BigInteger factorial(int num) {
+	static BigInteger factorial(int num) {
 		BigInteger fact = BigInteger.valueOf(1);
 		for (long i = 1; i<=num;i++) {
 			fact = fact.multiply(BigInteger.valueOf(i));
@@ -14,9 +14,18 @@ public class Puzzle {
 		return fact;
 	}
 	
-	private BigInteger numberOfPlaylist(int N, int K, int L) {
-		if ( N == K ) return BigInteger.valueOf(0);
-		return ((factorial(L-1)).divide((factorial(N-1).multiply(factorial(L-N)))));//.mod(BigInteger.valueOf(1000000007));
+	static int numberOfPlaylist(int N, int K, int L) {
+		if ( N == K ) return 0;
+		else if (N == K && K == L) return 1;
+		
+//		BigInteger temp1 = (((factorial(N)).divide((factorial(K).multiply(factorial(N-K))))));
+		BigInteger temp1 = (factorial(N)).multiply(factorial(N));
+		
+//		BigInteger temp2 = BigInteger.valueOf(N).pow(L-N);
+		BigInteger temp2 = (factorial(K)).multiply(factorial(N-K)).multiply(factorial(L-N)).multiply(factorial(2*N-L));
+		return (temp1.multiply(temp2)).mod(BigInteger.valueOf(1000000007)).intValue();
+//		return (((factorial(L-1)).divide((factorial(N-1).multiply(factorial(L-N))))).mod(BigInteger.valueOf(1000000007))).intValue();
+//		return (((factorial(L+N-K)).divide((factorial(N-1).multiply(factorial(L-K+1))))).mod(BigInteger.valueOf(1000000007))).intValue();
 	}
 	
 	public static void main(String[] args) {
@@ -24,7 +33,7 @@ public class Puzzle {
 		int N = s.nextInt();
 		int K = s.nextInt();
 		int L = s.nextInt();
-		Puzzle m = new Puzzle();
-		System.out.println(m.numberOfPlaylist(N, K, L));
+//		Puzzle m = new Puzzle();
+		System.out.println(Puzzle.numberOfPlaylist(N, K, L));
 	}
 }
